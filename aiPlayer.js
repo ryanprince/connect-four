98208e7bb669035f94364547fa19449bf5006d85
Ryan Prince <rnprince7@gmail.com>
2017-05-21 23:09:11 -0500

    improves minimax code

--- aiPlayer.js
+++ aiPlayer.js
@@ -21,37 +21,32 @@ function Player(name, symbol, strategy) {
   };
 
   const minimax = (board, depth, minimizingPlayer, minimizingPlayerSymbol) => {
-    if (depth >= MAX_DEPTH) {
+    // Base case: assign values for game states at the bottom of the recursion
+    if (depth >= MAX_DEPTH)
       return -1;
-    }
-
     let winningSymbol = board.winningPiece();
     if (winningSymbol === this.symbol)
       return 1;
-    else if (winningSymbol !== undefined)
+    else if (winningSymbol === minimizingPlayerSymbol)
       return -2;
     else if (board.isFull())
       return 0;
 
+    // Evaluate possible moves
     let moveCandidates = board.availableCols();
+    let moveSymbol = minimizingPlayer ? minimizingPlayerSymbol : this.symbol;
     let candidateVals = moveCandidates.map(moveCol => {
-      board.add(minimizingPlayer ? minimizingPlayerSymbol : this.symbol, moveCol);
+      board.add(moveSymbol, moveCol);
       let val = minimax(board, depth + 1, !minimizingPlayer, minimizingPlayerSymbol);
       board.pop(moveCol);
       return val;
     });
-
     let selectionFunction = minimizingPlayer ? Math.min : Math.max;
-
     let bestVal = selectionFunction(...candidateVals);
     let bestMove = moveCandidates[candidateVals.indexOf(bestVal)];
 
-    // Randomize selecting among equally weighted values.
-    let bestMoves = candidateVals.reduce((acc, val, i) => {
-      if (val === bestVal)
-        acc.push(moveCandidates[i]);
-      return acc;
-    }, []);
+    // If multiple moves are equally good, randomly select one
+    let bestMoves = moveCandidates.filter((move, i) => candidateVals[i] === bestVal);
     bestMove = bestMoves[Math.floor(Math.random() * bestMoves.length)];
 
     if (depth === 0)
